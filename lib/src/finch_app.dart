f79b722fde2a7c5bdc3466a4779b46edb828d5ea
Farhad <fardzia@gmail.com>
2025-11-16 03:26:05 +0100

    Update v1.0.1

--- lib/src/finch_app.dart
+++ lib/src/finch_app.dart
@@ -1058,5 +1058,5 @@ class _Info {
   /// - MINOR: New features (backward compatible)
   /// - PATCH: Bug fixes (backward compatible)
   /// - PRERELEASE: Pre-release identifiers (alpha, beta, rc)
-  final String version = '1.0.0';
+  final String version = '1.0.1';
 }
