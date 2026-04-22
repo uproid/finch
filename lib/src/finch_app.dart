dcc12b9e76719ecabd427aade063ca61c90a071a
Farhad <farhad@toomba.nl>
2026-04-23 00:13:54 +0200

    Fixed version name

--- lib/src/finch_app.dart
+++ lib/src/finch_app.dart
@@ -1189,5 +1189,5 @@ class _Info {
   /// - MINOR: New features (backward compatible)
   /// - PATCH: Bug fixes (backward compatible)
   /// - PRERELEASE: Pre-release identifiers (alpha, beta, rc)
-  final String version = '1.2.0';
+  final String version = '1.2.1';
 }
