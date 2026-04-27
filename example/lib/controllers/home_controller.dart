75d3a4d0452ac45c13fc9220f827986adb192442
Farhad <farhad@toomba.nl>
2026-04-27 21:10:46 +0200

    Added https to info

--- example/lib/controllers/home_controller.dart
+++ example/lib/controllers/home_controller.dart
@@ -415,7 +415,7 @@ class HomeController extends Controller {
         'URI': rq.uri.path,
         'Email default': configs.mailDefault,
         'IP': rq.getIP(),
-        'https': rq.uri.scheme
+        'https': rq.httpRequest.uri.scheme
       },
       'Headers': headers,
       'Versions': {
