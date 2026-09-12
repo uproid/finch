1ba7ecbd04347005700ac0174630f77d95606da5
Farhad <farhad@toomba.nl>
2026-09-12 19:27:19 +0200

    public runCommands for FinchApp

--- lib/src/finch_app.dart
+++ lib/src/finch_app.dart
@@ -298,7 +298,7 @@ class FinchApp {
 
     CappConsole.write(welcomeFinch.join('\n'));
 
-    await _runCommands(_args);
+    await runCommands(_args);
   }
 
   final _helpOption = CappOption(
@@ -811,8 +811,13 @@ class FinchApp {
   /// Otherwise, it starts an interactive command prompt (Finch>) where
   /// users can continue entering commands.
   /// [args] - List of command-line arguments to process
-  Future<void> _runCommands(List<String> args) async {
+  Future<void> runCommands(List<String> args, [bool direct = false]) async {
     if (args.isEmpty) {
+      return;
+    }
+
+    if (direct) {
+      await _getCommandManager(args).process(newArgs: args);
       return;
     }
 
